c21ba50a46a2581d973055ec95636cf992dff47b
reshmi-02 <reshmihash123@gmail.com>
2024-01-09 16:32:34 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+var num1=parseInt(prompt("Enter the first number"))
+document.write("given number 1 ="+num1+"<br><br>")
+var num2=parseInt(prompt("Enter the second number"))
+document.write("given number 2 ="+num2+"<br><br>")
+
+num1=num1+num2;
+num2=num1-num2;
+num1=num1-num2
+document.write("num1 = "+num1+"<br>")
+document.write("num2 = "+num2+"<br>")
